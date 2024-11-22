a82d871fd88f50fa9757e0653398d0173dee2775
hwangTaehyeon <clowns1232@gmail.com>
2024-11-22 14:22:29 +0900

    delete와 Post 추가 1. 기존 Post에서 db에 데이터 안넣는 문제 수정 2. delete 추가 3. delete및 post 후 get으로 가져올 때 적용된거 확인 완료

--- routes/book.js
+++ routes/book.js
@@ -5,23 +5,36 @@ var router = express.Router();
 var dbSize = db.size;
 
 router.get("/", function (req, res) {
+  if (dbSize < 0) res.json({ message: "책이 존재하지 않습니다." });
   const dbArray = Array.from(db.values());
+  const dbObj = {};
+  dbArray.forEach((data) => {
+    dbObj[data.id] = data;
+  });
 
-  res.json(dbArray);
+  res.json(dbObj);
 });
 
 router.get("/:id", function (req, res) {
   const { id } = req.params;
-  if (id > dbSize) res.json({ message: "존재하지 않는 책 입니다." });
 
   const dbArray = Array.from(db.values());
+  const isExist =
+    dbArray.findIndex((data) => {
+      return parseInt(data.id) === parseInt(id);
+    }) > -1;
+
+  if (isExist === false)
+    return res.json({ message: "존재하지 않는 책 입니다." });
 
   res.json(dbArray[id - 1]);
 });
 
 router.post("/", function (req, res) {
   const { title, subTitle, description, author } = req.body;
-  const id = dbSize + 1;
+  const dbArray = Array.from(db.values());
+
+  const id = dbArray.pop().id + 1;
 
   if ((title && subTitle && description && author) === undefined)
     res.json({ message: "데이터가 부족합니다." });
@@ -34,7 +47,34 @@ router.post("/", function (req, res) {
     author,
   };
 
+  db.set(id, data);
+
   res.json(data);
+});
+
+router.delete("/:id", function (req, res) {
+  const { id } = req.params;
+  const dbArray = Array.from(db.values());
+  const isExist = dbArray.find((data) => data.id === id);
+
+  if (isExist) res.json({ message: "존재하지 않는 책 입니다." });
+
+  const filterDbArray = dbArray.filter((data) => {
+    return parseInt(data.id) !== parseInt(id);
+  });
+  const dbObj = {};
+  dbArray.forEach((data) => {
+    dbObj[data.id] = data;
+  });
+
+  db.clear();
+  filterDbArray.forEach((data, index) => {
+    db.set(index, data);
+  });
+
+  res.json({
+    message: "데이터가 삭제되었습니다.",
+  });
 });
 
 module.exports = router;
